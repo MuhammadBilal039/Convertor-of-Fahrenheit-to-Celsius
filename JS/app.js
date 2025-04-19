78f5b3623e62d1c89acbc72349c8d72629aa936f
Muhammad Bilal <150532599+MuhammadBilal039@users.noreply.github.com>
2025-04-19 10:21:08 -0700

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,23 @@
+function converter() {
+  const textElement = document.getElementById('text').value;
+  const type = document.getElementById('types').value;
+  const resultDiv = document.getElementById('result');
+  let result = "";
+
+  if (isNaN(textElement)) {
+    resultDiv.innerText = 'Please enter a valid number.';
+    return;
+  }
+
+  if (type === 'fahrenheit') {
+    const celisus = ((textElement - 32) * 5) / 9;
+    result = textElement + '°F = ' + celisus.toFixed(2) + '°C';
+  } else {
+    const fahrenheit = (textElement * 9) / 5 + 32;
+    result = textElement + '°C = ' + fahrenheit.toFixed(2) + '°F';
+  }
+
+  resultDiv.innerText = result;
+}
+
 
